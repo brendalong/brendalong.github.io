8808a6e645bd682a56ff8fac24e99cbffd59f73d
Brenda Long <brenda@studio6thave.com>
2021-02-02 16:44:18 -0600

    new stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Brenda's Github.io V3</h2>
+      <h2>Brenda's Github.io V4</h2>
+
+      <h3>A Few Projects</h3>
+      <ul>
+        <li><a href="https://brendalong.github.io/poke-binder">Poke-Binder</a></li>
+        <li><a href="https://brendalong.github.io/christmas-list">A Christmas List</a></li>
+      </ul>
     </div>
   );
 }
