5e9bd222250e3f0988c3674382530adc07c12f8c
Brenda Long <brenda@studio6thave.com>
2021-02-02 17:28:54 -0600

    added project links

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
       <h2>Brenda's Github.io V4</h2>
 
       <h3>A Few Projects</h3>
+      <div class="list">
       <ul>
-        <li><a href="https://brendalong.github.io/poke-binder">Poke-Binder</a></li>
-        <li><a href="https://brendalong.github.io/christmas-list">A Christmas List</a></li>
+        <li>🌮 &nbsp;<a href="https://brendalong.github.io/poke-binder">Poke-Binder</a></li>
+        <li>🌮 &nbsp;<a href="https://brendalong.github.io/christmas-list">A Christmas List</a></li>
       </ul>
+      </div>
     </div>
   );
 }
